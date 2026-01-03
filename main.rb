ff8ccbc7112405188545d163f210b01abb1d9954
David H <151378521+Kezy-d4@users.noreply.github.com>
2026-01-03 18:13:59 -0500

    feat: add output comments

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require_relative 'lib/chess'
 
-pp Chess::Board.new.hello
-pp Chess::Square.new.hello
-pp Chess::Board.new.new_square
-pp Chess::Board.sum(1, 1)
+pp Chess::Board.new.hello # => "hello from Board"
+pp Chess::Square.new.hello # => "hello from Square"
+pp Chess::Board.new.new_square # => #<Chess::Square:0x00007f4762d5d850>
+pp Chess::Board.sum(1, 1) # => 2
